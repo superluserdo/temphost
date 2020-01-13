5b5c1dbb6a07efaeae27cd08c9e9b18d9ab601dd
superluserdo <superluserdo@gmail.com>
2020-01-13 22:25:01 +0000

    Bump minor version

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(name='temphost',
-    version='0.1.0',
+    version='0.2.0',
     scripts=['temphost'],
     data_files = [
             ('share/temphost', ['index.html.template', 'style.css'])
             ],
-    #packages=find_packages(),
-    #include_package_data=True,
     )
